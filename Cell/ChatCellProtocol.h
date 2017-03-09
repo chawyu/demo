1303893812b93f095664fe275dbaa4aa91529638
周维 <Just@zhouweideMacBook-Air.local>
2017-03-10 01:04:46 +0800

    basecell

---
+++ Cell/ChatCellProtocol.h
@@ -0,0 +1,13 @@
+//
+//  ChatCellProtocol.h
+//  iOSDemo
+//
+//  Created by 周维 on 17/3/9.
+//  Copyright © 2017年 chaw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ChatCellProtocol <NSObject>
+
+@end
